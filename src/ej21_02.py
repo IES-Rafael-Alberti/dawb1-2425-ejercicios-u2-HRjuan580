6382377ef952fc2bc27503e489cd588272d69c59
HRjuan580 <hrjuan@iesrafaelalberti.es>
2024-10-21 19:53:59 +0200

    1 y 2 hechos

--- src/ej21_02.py
+++ src/ej21_02.py
@@ -5,14 +5,21 @@ Escribir un programa que almacene la cadena de caracteres contraseña en una var
 
 
 def almacenar_contraseña():
+    return "contraseña"
 
+
+def verificar_contraseña(contraseña_guardada, contraseña_usuario):
+    return contraseña_guardada.lower() == contraseña_usuario.lower()
 
 
 def main():
-
-
-
-
+    contraseña_guardada = almacenar_contraseña()
+    contraseña_usuario = input("Introduce la contraseña: ")
+    
+    if verificar_contraseña(contraseña_guardada, contraseña_usuario):
+        print("La contraseña es correcta.")
+    else:
+        print("La contraseña es incorrecta.")
 
 
 if __name__ == "__main__":
