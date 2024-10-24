a2da057b30c4ba3eb3f0e07e7e8a1a35835373db
HRjuan580 <hrjuan@iesrafaelalberti.es>
2024-10-24 21:21:16 +0200

    nuevo

--- src/ej22_08.py
+++ src/ej22_08.py
@@ -8,3 +8,43 @@ Escribir un programa que pida al usuario un número entero y muestre por pantall
 9 7 5 3 1
 """
 
+
+
+def pedir_numero(n):
+    while True:
+        try:
+            n = int(input("Introduce un numero entero: "))
+            if n > 0:
+                return n
+            else:
+             print("Por favor, introduce un numero entero valido")
+        except ValueError:
+            print("Entrada no valida. Hazlo de nuevo")
+
+
+def construir_fila(inicio: int) -> str:
+    for i in range(inicio, 0, -2):
+        fila += str
+            
+
+
+def triangulo_rectangulo(n: int) -> str:
+    cadena = ""
+    triangulo = ''
+    if num%2 != 0:
+        for i in range(1, n1, 2):
+            cadena += str(1) +
+
+
+            
+            
+def main():
+    n = pedir_numero("Introduce un numero entero")
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
